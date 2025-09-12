864aba065649da80cc2540ff08cb9208fa04dc04
LUIS_RODRIGUEZ <luisdavidrodriguezv@gmail.com>
2025-09-11 18:37:22 -0600

    prune comments

--- explorenasaclientapp/src/env.js
+++ explorenasaclientapp/src/env.js
@@ -1,10 +1,9 @@
-// const APOD_BASE = `https://api.nasa.gov/planetary/apod?api_key=${KEY}`;
 const KEY = 'ZF68DPHnWhdvWZRpqSUtvTfLOXMiwvOnHCQRLCZL';
 const LOCAL_APODS_BASE = 'https://localhost:7018/api/Apods';
 const LOCAL_ROVER_PHOTOS = 'https://localhost:7018/api/Rovers';
 const LOCAL_ROVER_MANIFEST = 'https://localhost:7018/api/Rovers/Manifest';
 const LOCAL_MEDIA = 'https://localhost:7018/api/Media';
-// eslint-disable-next-line import/prefer-default-export
+
 export {
   LOCAL_APODS_BASE, KEY, LOCAL_ROVER_PHOTOS, LOCAL_ROVER_MANIFEST, LOCAL_MEDIA,
 };
